2a2ffe2ddb9f23b9c4ece5ebc1c663bcc1261d9c
VainaviS <75711421+VainaviS@users.noreply.github.com>
2024-02-11 20:13:19 +0530

    placement changes

--- src/main.js
+++ src/main.js
@@ -1,37 +1,37 @@
 tailwind.config = {
-            theme: {
-                extend: {
-                    fontFamily: {
-                        inter: ['Inter', 'sans-serif'],
-                    },
-                    animation: {
-                        'infinite-scroll': 'infinite-scroll 25s linear infinite',
-                    },
-                    keyframes: {
-                        'infinite-scroll': {
-                            from: { transform: 'translateX(0)' },
-                            to: { transform: 'translateX(-100%)' },
-                        }
-                    }                    
-                },
-            },
-        };
+  theme: {
+      extend: {
+          fontFamily: {
+              inter: ['Inter', 'sans-serif'],
+          },
+          animation: {
+              'infinite-scroll': 'infinite-scroll 25s linear infinite',
+          },
+          keyframes: {
+              'infinite-scroll': {
+                  from: { transform: 'translateX(0)' },
+                  to: { transform: 'translateX(-100%)' },
+              }
+          }                    
+      },
+  },
+};
 
-        // JavaScript to handle menu toggle and dropdown
+// JavaScript to handle menu toggle and dropdown
 
-        $(".custom-carousel").owlCarousel({
-          autoWidth: true,
-          loop: true
-        });
-        $(document).ready(function () {
-          $(".custom-carousel .item").click(function () {
-            $(".custom-carousel .item").not($(this)).removeClass("active");
-            $(this).toggleClass("active");
-          });
-        });
-        document.addEventListener('DOMContentLoaded', function() {
-          new GreenAudioPlayer('.example');
-        });
+$(".custom-carousel").owlCarousel({
+autoWidth: true,
+loop: true
+});
+$(document).ready(function () {
+$(".custom-carousel .item").click(function () {
+  $(".custom-carousel .item").not($(this)).removeClass("active");
+  $(this).toggleClass("active");
+});
+});
+document.addEventListener('DOMContentLoaded', function() {
+new GreenAudioPlayer('.example');
+});
 
 // Nav Bar
 
@@ -40,130 +40,130 @@ const toggleBtnIcon = toggleBtn.querySelector('i');
 const dropDownMenu = document.querySelector('.dropdown-menu');
 
 toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle('open');
-  const isOpen = dropDownMenu.classList.contains('open');
-  toggleBtnIcon.classList.toggle('fa-bars', !isOpen);
-  toggleBtnIcon.classList.toggle('fa-xmark', isOpen);
+dropDownMenu.classList.toggle('open');
+const isOpen = dropDownMenu.classList.contains('open');
+toggleBtnIcon.classList.toggle('fa-bars', !isOpen);
+toggleBtnIcon.classList.toggle('fa-xmark', isOpen);
 };
 
 // Nav Bar Ends
-        
+
 document.addEventListener('DOMContentLoaded', function () {
-  const menuToggle = document.getElementById('menu-toggle');
-  const menuClose = document.getElementById('menu-close');
-  const menuDropdown = document.getElementById('menu-dropdown');
+const menuToggle = document.getElementById('menu-toggle');
+const menuClose = document.getElementById('menu-close');
+const menuDropdown = document.getElementById('menu-dropdown');
 
-  menuToggle.addEventListener('click', function () {
-      menuDropdown.classList.toggle('hidden');
-  });
+menuToggle.addEventListener('click', function () {
+menuDropdown.classList.toggle('hidden');
+});
 
-  menuClose.addEventListener('click', function () {
-      menuDropdown.classList.add('hidden');
-  });
+menuClose.addEventListener('click', function () {
+menuDropdown.classList.add('hidden');
+});
 });
 
 
 VanillaTilt.init(document.querySelectorAll(".card"), {
-  max: 25,
-  speed: 400,
-  glare: true,
-  "max-glare": 0.5
+max: 25,
+speed: 400,
+glare: true,
+"max-glare": 0.5
 });
 
 
-        document.addEventListener('DOMContentLoaded', function () {
-          
-    new Splide('#audio-carousel', {
-      type       : 'fade',
-      heightRatio: 0.5,
-      pagination : false,
-      cover      : true,
-      breakpoints: {
-        600: {
-          heightRatio: 0.3,
-        },
-      },
-    }).mount();
-  });
-  const box = document.querySelector('.box');
-  
+document.addEventListener('DOMContentLoaded', function () {
+
+new Splide('#audio-carousel', {
+type       : 'fade',
+heightRatio: 0.5,
+pagination : false,
+cover      : true,
+breakpoints: {
+600: {
+heightRatio: 0.3,
+},
+},
+}).mount();
+});
+const box = document.querySelector('.box');
+
 
 setInterval(setBorderRadius, 300);
 
 window.addEventListener('scroll', () => {
-  const section = document.getElementById('animated-section');
-  const div = document.getElementById('animated-div');
-  const sectionPosition = section.getBoundingClientRect().top;
-  const divPosition = div.getBoundingClientRect().top;
-  const screenHeight = window.innerHeight;
+const section = document.getElementById('animated-section');
+const div = document.getElementById('animated-div');
+const sectionPosition = section.getBoundingClientRect().top;
+const divPosition = div.getBoundingClientRect().top;
+const screenHeight = window.innerHeight;
 
-  if (sectionPosition < screenHeight / 2) {
-    section.style.animation = 'slide-up 1s ease forwards';
-  }
-  if (divPosition < screenHeight / 2) {
-    div.style.animation = 'slide-up 1s ease forwards';
-  }
+if (sectionPosition < screenHeight / 2) {
+section.style.animation = 'slide-up 1s ease forwards';
+}
+if (divPosition < screenHeight / 2) {
+div.style.animation = 'slide-up 1s ease forwards';
+}
 });
 
 function setBorderRadius() {
-	box.style.setProperty('--br-blobby', generateBorderRadiusValue());
-	box.style.setProperty('--br-blobby-after', generateBorderRadiusValue());
-	box.style.setProperty('--br-blobby-before', generateBorderRadiusValue());
+box.style.setProperty('--br-blobby', generateBorderRadiusValue());
+box.style.setProperty('--br-blobby-after', generateBorderRadiusValue());
+box.style.setProperty('--br-blobby-before', generateBorderRadiusValue());
 }
 
 function generateBorderRadiusValue() {
-	return `${getRandomValue()}% ${getRandomValue()}% ${getRandomValue()}% ${getRandomValue()}% / ${getRandomValue()}% ${getRandomValue()}% ${getRandomValue()}%`;
+return `${getRandomValue()}% ${getRandomValue()}% ${getRandomValue()}% ${getRandomValue()}% / ${getRandomValue()}% ${getRandomValue()}% ${getRandomValue()}%`;
 }
-	
+
 function getRandomValue() {
-	return Math.floor(Math.random() * 50) + 50;
+return Math.floor(Math.random() * 50) + 50;
 }
 
 document.getElementById('next').onclick = function(){
-  const widthItem = document.querySelector('.item').offsetWidth;
-  document.getElementById('formList').scrollLeft += widthItem;
+const widthItem = document.querySelector('.item').offsetWidth;
+document.getElementById('formList').scrollLeft += widthItem;
 }
 document.getElementById('prev').onclick = function(){
-  const widthItem = document.querySelector('.item').offsetWidth;
-  document.getElementById('formList').scrollLeft -= widthItem;
+const widthItem = document.querySelector('.item').offsetWidth;
+document.getElementById('formList').scrollLeft -= widthItem;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  hljs.initHighlightingOnLoad();
+hljs.initHighlightingOnLoad();
 
-  const codeBlock = document.getElementById('code');
-  const copyButton = document.getElementById('copy-button');
-  const copySuccess = document.getElementById('copy-success');
+const codeBlock = document.getElementById('code');
+const copyButton = document.getElementById('copy-button');
+const copySuccess = document.getElementById('copy-success');
 
-  const copyTextHandler = () => {
-    const text = codeBlock.innerText;
+const copyTextHandler = () => {
+const text = codeBlock.innerText;
 
-    // first version - document.execCommand('copy');
-    // var element = document.createElement('textarea');
-    // document.body.appendChild(element);
-    // element.value = text;
-    // element.select();
-    // document.execCommand('copy');
-    // document.body.removeChild(element);
+// first version - document.execCommand('copy');
+// var element = document.createElement('textarea');
+// document.body.appendChild(element);
+// element.value = text;
+// element.select();
+// document.execCommand('copy');
+// document.body.removeChild(element);
 
-    // copySuccess.classList.add('show-message');
-    // setTimeout(() => {
-    //   copySuccess.classList.remove('show-message');
-    // }, 2500);
+// copySuccess.classList.add('show-message');
+// setTimeout(() => {
+//   copySuccess.classList.remove('show-message');
+// }, 2500);
 
-    //   second version - Clipboard API
-    navigator.clipboard.writeText(text).then(
-      () => {
-        copySuccess.classList.add('show-message');
-        setTimeout(() => {
-          copySuccess.classList.remove('show-message');
-        }, 2500);
-      },
-      () => {
-        console.log('Error writing to the clipboard');
-      }
-    );
-  };
+//   second version - Clipboard API
+navigator.clipboard.writeText(text).then(
+() => {
+copySuccess.classList.add('show-message');
+setTimeout(() => {
+copySuccess.classList.remove('show-message');
+}, 2500);
+},
+() => {
+console.log('Error writing to the clipboard');
+}
+);
+};
 
-  copyButton.addEventListener('click', copyTextHandler);
+copyButton.addEventListener('click', copyTextHandler);
 });
